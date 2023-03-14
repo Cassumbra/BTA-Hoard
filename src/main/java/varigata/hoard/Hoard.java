c32f3009a44e7c7141d9bd23a53a3215801e945a
Cassumbra <penumbruh@gmail.com>
2023-03-13 20:27:09 -0500

    basic steeldollar layering

--- src/main/java/varigata/hoard/Hoard.java
+++ src/main/java/varigata/hoard/Hoard.java
@@ -2,20 +2,38 @@ package varigata.hoard;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.src.Block;
+import net.minecraft.src.BlockLayerBase;
+import net.minecraft.src.Item;
+import net.minecraft.src.ItemBlock;
 import net.minecraft.src.Material;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.helper.BlockHelper;
+import turniplabs.halplibe.mixin.accessors.BlockAccessor;
 
 
 public class Hoard implements ModInitializer {
     public static final String MOD_ID = "hoard";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static final Item pileSteeldollar = (new ItemPileSteeldollar(140)).setIconCoord(8, 3).setItemName("pile.steeldollar");
+
     public static final Block steeldollarBlock = BlockHelper.createBlock(MOD_ID, new Block(900, Material.sand), "steeldollar", "steeldollar_block.png", Block.soundGravelFootstep, 0.1f, 0.1f, 0.0f);
+    //public static final Block layerSteeldollar = (new BlockLayerSteeldollar(901, Material.sand)).setBlockName("layer.steeldollar").setTexCoords(2, 4).setPlaceOverwrites();//.setIsLitInteriorSurface(true);
+    public static final Block layerSteeldollar = new BlockLayerSteeldollar(901, Material.sand).setBlockName("layer.steeldollar").setTexCoords(2, 4).setPlaceOverwrites();//.setNotInCreativeMenu();
+
+    static {
+        ((BlockAccessor) layerSteeldollar).callSetHardness(0.1f);
+        ((BlockAccessor) layerSteeldollar).callSetStepSound(Block.soundGlassFootstep);
+        ((BlockLayerBase) layerSteeldollar).setFullBlockID(steeldollarBlock.blockID);
+    }
+
+
 
     @Override
     public void onInitialize() {
         LOGGER.info("Hoard initialized.");
+        //Item.itemsList[pileSteeldollar.itemID] = pileSteeldollar;
+        Item.itemsList[layerSteeldollar.blockID] = new ItemBlock(layerSteeldollar.blockID - Block.blocksList.length);
     }
 }
---
+++ src/main/java/varigata/hoard/ItemPileSteeldollar.java
@@ -0,0 +1,65 @@
+package varigata.hoard;
+
+import net.minecraft.src.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static varigata.hoard.Hoard.MOD_ID;
+
+public class ItemPileSteeldollar extends Item {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public ItemPileSteeldollar(int i) {
+        super(i);
+    }
+
+    public boolean onItemUse(ItemStack itemstack, EntityPlayer entityplayer, World world, int i, int j, int k, int l, double heightPlaced) {
+        if (world.getBlockId(i, j, k) == Hoard.layerSteeldollar.blockID && l == 1) {
+            itemstack.consumeItem(entityplayer);
+            ((BlockLayerSteeldollar)Hoard.layerSteeldollar).accumulate(world, i, j, k );
+
+            return true;
+        }
+
+        if (world.getBlockId(i, j, k) != Block.layerSnow.blockID) {
+            if (l == 0) {
+                --j;
+            }
+
+            if (l == 1) {
+                ++j;
+            }
+
+            if (l == 2) {
+                --k;
+            }
+
+            if (l == 3) {
+                ++k;
+            }
+
+            if (l == 4) {
+                --i;
+            }
+
+            if (l == 5) {
+                ++i;
+            }
+
+            if (!world.isAirBlock(i, j, k)) {
+                return false;
+            }
+        }
+
+        if (Hoard.layerSteeldollar.canPlaceBlockAt(world, i, j, k)) {
+            itemstack.consumeItem(entityplayer);
+            // TODO: Add placement sound
+            //world.playSoundEffect((double)((float)i + 0.5F), (double)((float)j + 0.5F), (double)((float)k + 0.5F), Block.wireRedstone.stepSound.func_1145_d(), (Block.wireRedstone.stepSound.getVolume() + 1.0F) / 2.0F, Block.wireRedstone.stepSound.getPitch() * 0.8F);
+            world.setBlockWithNotify(i, j, k, Hoard.layerSteeldollar.blockID);
+        }
+
+        return true;
+
+    }
+}
