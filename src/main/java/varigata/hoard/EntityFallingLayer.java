25eda05718fca84feb714a7280a26ee72fba5d6b
Cassumbra <penumbruh@gmail.com>
2023-04-01 19:32:10 -0500

    fix ghostblock issues in multiplayer

--- src/main/java/varigata/hoard/EntityFallingLayer.java
+++ src/main/java/varigata/hoard/EntityFallingLayer.java
@@ -74,10 +74,14 @@ public class EntityFallingLayer extends EntityFallingSand {
                 this.motionY *= -0.5;
                 this.setEntityDead();
 
-                if ((!this.worldObj.canBlockBePlacedAt(this.blockID, i, j, k, true, 1) || (BlockHoardLayerBase.canFallBelow(this.worldObj, i, j - 1, k) && this.worldObj.getBlockId(i, j-1, k) != this.blockID)) && !this.worldObj.isMultiplayerAndNotHost) {
-                    // TODO: Make this drop the proper thingies
-                    //int pileID = ((BlockHoardLayerBase)Block.getBlock(blockID)).pileID;
-                    this.dropItem(this.pileID, metadata + 1);
+                boolean canPlaceBlock = this.worldObj.canBlockBePlacedAt(this.blockID, i, j, k, true, 1);
+                boolean canFallBelow = BlockHoardLayerBase.canFallBelow(this.worldObj, i, j - 1, k);
+                boolean blockMatches = this.worldObj.getBlockId(i, j-1, k) != this.blockID;
+
+                if ((!canPlaceBlock || (canFallBelow && blockMatches))) {
+                    if (!this.worldObj.isMultiplayerAndNotHost) {
+                        this.dropItem(this.pileID, metadata + 1);
+                    }
                 } else {
                     boolean trySetBlock = this.worldObj.setBlockAndMetadataWithNotify(i, j, k, this.blockID, this.metadata);
 
@@ -91,9 +95,28 @@ public class EntityFallingLayer extends EntityFallingSand {
                     }
                 }
 
-                //else {
-                //    this.worldObj.setBlockAndMetadataWithNotify(i, j, k, this.blockID, this.metadata);
-                //}
+                /*
+                if ((!canPlaceBlock || (canFallBelow && blockMatches)) && !this.worldObj.isMultiplayerAndNotHost) {
+                    this.dropItem(this.pileID, metadata + 1);
+                } else {
+
+                    boolean trySetBlock = false;
+                    if (!(!canPlaceBlock || (canFallBelow && blockMatches))) {
+                        trySetBlock = this.worldObj.setBlockAndMetadataWithNotify(i, j, k, this.blockID, this.metadata);
+                    }
+
+                    if (trySetBlock) {
+                        if (this.worldObj.getBlockId(i, j - 1, k) == this.blockID) {
+                            BlockHoardLayerBase.giveLayers(this.worldObj, i, j, k, i, j-1, k);
+                        }
+                        if (this.worldObj.getBlockMetadata(i, j, k) == 7) {
+                            this.worldObj.setBlockWithNotify(i, j, k, this.fullBlockID);
+                        }
+                    }
+                }
+                 */
+
+
             } else if (this.fallTime > 100 && !this.worldObj.isMultiplayerAndNotHost) {
                 // TODO: Make this drop the proper thingies
 
